02815372a66db724eab2af75f29b0e3e0dd31a19
YaroslavLitvinov <yaroslav.litvinov@gmail.com>
2015-11-20 02:35:18 +0000

    more accurate merging of structure datatypes inside lists

--- merge_schemas.py
+++ merge_schemas.py
@@ -10,6 +10,8 @@ def get_branches_from_schema_recursively(schema):
     for key, value in schema.iteritems():
         if type(value) is dict:
             l = get_branches_from_schema_recursively(value)
+            if not len(l):
+                branches.append(key)
             for item in l:
                 branches.append(key+'.'+item)
         elif type(value) is list:
@@ -18,6 +20,8 @@ def get_branches_from_schema_recursively(schema):
             except:
                 message("Data type not specified. Empty arrays like [] not allowed")
                 raise
+            if not len(l):
+                branches.append(key)
             for item in l:
                 branches.append(key+'.'+item)
         else:
@@ -27,7 +31,7 @@ def get_branches_from_schema_recursively(schema):
 
 def update_data_on_branch_recursively(schema, branch, new_value):
     if type(schema) is not list and type(schema) is not dict:
-        return schema
+        return new_value
 
     updated_schema = {}
     for key, value in schema.iteritems():
@@ -38,7 +42,12 @@ def update_data_on_branch_recursively(schema, branch, new_value):
         if type(value) is list:
             #do not propogate array of empty structs
             if not(len(value)>0 and type(value[0]) is dict and len(value[0]) == 0):
-                updated_schema[key] = [update_data_on_branch_recursively( value[0], '.'.join(branch.split('.')[1:]), new_value )]
+                res = update_data_on_branch_recursively( value[0], '.'.join(branch.split('.')[1:]), new_value )
+                if type(res) is list:
+                    updated_schema[key] = res
+                else:
+                    updated_schema[key] = [res]
+                
         elif type(value) is dict:
             #do not propogate empty structs
             if len(value) != 0:
@@ -62,7 +71,8 @@ def get_data_from_branch_recursively(schema, branch):
 
         if type(value) is list:
             if key == branch:
-                res = value[0]
+                #res = value[0]
+                res = value
             #do not propogate array of empty structs
             elif not(len(value)>0 and type(value[0]) is dict and len(value[0]) == 0):
                 res = get_data_from_branch_recursively( value[0], '.'.join(branch.split('.')[1:]) )
@@ -92,16 +102,21 @@ if args.primary_schema is None or args.secondary_schema is None or args.merged_s
     exit(1)
 
 primary_schema = json.load(args.primary_schema)
+print 0
 secondary_schema = json.load(args.secondary_schema)
 
+print 1
 primary_branches = get_branches_from_schema_recursively(primary_schema)
-secondary_branches = get_branches_from_schema_recursively(secondary_schema)
+print primary_branches
+print 2
 
 new_schema_with_derived_datatypes = copy.copy(primary_schema)
-
+print 3
 for branch in primary_branches:
     data_primary = get_data_from_branch_recursively(primary_schema, branch)
     data_secondary = get_data_from_branch_recursively(secondary_schema, branch)
+    if branch == "options.groupings.items.reference_documents":
+        pass
     if data_primary is not None and data_secondary is not None \
             and data_secondary != data_primary \
             and data_secondary != 'TINYINT':
@@ -110,7 +125,7 @@ for branch in primary_branches:
             update_data_on_branch_recursively(new_schema_with_derived_datatypes, 
                                               branch, data_secondary)
 
-json.dump(new_schema_with_derived_datatypes, args.merged_schema, indent=4)
+json.dump(new_schema_with_derived_datatypes, args.merged_schema, indent=4, sort_keys=True)
 
 
 
